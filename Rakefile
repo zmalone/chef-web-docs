9112e0f08d856740a04802decd806b6b86a58b7b
Seth Vargo <sethvargo@gmail.com>
2013-02-06 17:40:39 -0500

    Yay images in PDFs

--- Rakefile
+++ Rakefile
@@ -10,10 +10,13 @@ task :build do
   FileUtils.rm_rf('./build')
   FileUtils.mkdir_p('./build')
 
+  # Copy over assets
+  FileUtils.cp_r('./source/assets', './build/assets')
+
   options = {
     header_line: true,
     header_left: 'Chef Quick Start Guide',
-    header_right: "Prepared on #{Time.now.strftime('%x')}",
+    header_right: "Build ##{Time.now.to_i}",
     header_spacing: 5,
     header_font_size: 8
   }
@@ -29,4 +32,19 @@ task :build do
 
     puts "[INFO] Successfully built '#{file}'"
   end
+
+  # Dir['source/**/*.md'].each do |file|
+  #   contents = Markdown.parse(File.read(file))
+
+  #   p contents.class
+
+  #   new_path = File.expand_path(file).sub('/source', '/build').gsub('.md', '.html')
+  #   FileUtils.mkdir_p new_path.split('/')[0...-1].join('/')
+
+  #   File.open(new_path, 'w') do |f|
+  #     f.write contents
+  #   end
+
+  #   puts "[INFO] Successfully built '#{new_path}'"
+  # end
 end
--- lib/markdown.rb
+++ lib/markdown.rb
@@ -13,17 +13,39 @@ module Markdown
       HTML::Pipeline::MarkdownFilter,
       HTML::Pipeline::AutolinkFilter,
       HTML::Pipeline::ImageMaxWidthFilter,
-      HTML::Pipeline::MentionFilter,
-      HTML::Pipeline::EmojiFilter,
-      HTML::Pipeline::SyntaxHighlightFilter
+      HTML::Pipeline::SyntaxHighlightFilter,
+      HTML::Pipeline::AbsoluteImageFilter
     ], context)
   end
 
   def context
     {
-      asset_root: 'http://your-domain.com/where/your/images/live/icons',
-      base_url:   'http://your-domain.com',
+      asset_root: File.expand_path('source/assets'),
+      base_url:   File.expand_path('build/assets'),
       gfm:        true
     }
+  end
+end
+
+
+require 'uri'
+
+module HTML
+  class Pipeline
+    class AbsoluteImageFilter < Filter
+      def call
+        doc.search('img').each do |img|
+          unless img['src'].nil?
+            src = img['src'].strip
+
+            if src.start_with? '.'
+              img['src'] = File.expand_path File.join(context[:base_url], src)
+            end
+          end
+        end
+
+        doc
+      end
+    end
   end
 end
