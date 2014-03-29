d762987e00a90d6c7accb7baaf56b2b25817018a
Nathan L Smith <nlloyds@gmail.com>
2014-03-28 22:31:06 -0500

    icons for tab helpers

--- lib/helpers/tab_helpers.rb
+++ lib/helpers/tab_helpers.rb
@@ -1,6 +1,16 @@
 require 'active_support/inflector'
 
 # Helper methods for creating tabs
+#
+# Helpers that build big old DOM trees with content_tag methods are terrible.
+# These only exist because Middleman does not support passing blocks to partials.
+#
+# The preferred way to do these would be something like:
+#
+#     <%= partial 'common/tab' do %>your stuff<% end %>
+#
+# and have those partials just use regular ERB to wrap the content passed into
+# them.
 module TabHelpers
   delegate :parameterize, to: ActiveSupport::Inflector
 
@@ -14,12 +24,16 @@ module TabHelpers
     )
   end
 
-  def tab(title, &block)
+  def tab(title, options = {}, &block)
     concat(
       content_tag(:section) do
         content_tag(:p, 'class'              => 'title',
                         'data-section-title' => true) do
-          link_to(title, "##{parameterize(title)}")
+          tab_title_text = title
+          if options[:icon]
+            tab_title_text = "<i class='icon-#{options[:icon]}'></i> #{tab_title_text}"
+          end
+          link_to tab_title_text, "##{parameterize(title)}"
         end
         content_tag(:div, 'class'                => 'content',
                           'data-section-content' => true,
