0a2f7156316d43dd65d1dc2e5a93261a21c3b5b4
tpetchel <tpetchel@gmail.com>
2014-06-13 12:39:22 -0700

    Migrate to Font Awesome 4.1 + Reference .css file in _icons.scss + Update all icon references in the content + Used some improved icons from 4.1

--- lib/markdown.rb
+++ lib/markdown.rb
@@ -45,14 +45,14 @@ module ZurbFoundation
   end
 
   def alerts
-    content.gsub!(/<p>\[INFO\] (.+)<\/p>/)     { "<div class=\"alert-box\"><i class=\"icon-exclamation-sign\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[SUCCESS\] (.+)<\/p>/)  { "<div class=\"alert-box success\"><i class=\"icon-ok-sign\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[WARN\] (.+)<\/p>/)     { "<div class=\"alert-box alert\"><i class=\"icon-warning-sign\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[NOTE\] (.+)<\/p>/)     { "<div class=\"alert-box secondary\"><i class=\"icon-info-sign\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[DOCS\] (.+)<\/p>/)     { "<div class=\"alert-box docs\"><i class=\"icon-book\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[CONCEPT\] (.+)<\/p>/)  { "<div class=\"alert-box concept\"><i class=\"icon-info-sign blueiconcolor\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[SIDEBAR\] (.+)<\/p>/)  { "<div class=\"alert-box sidebar\"><i class=\"icon-comment blueiconcolor icon-2x\"></i> #{$1}</div>" }
-    content.gsub!(/<p>\[COMMENT\] (.+)<\/p>/)  { "<div class=\"alert-box comment\"><i class=\"icon-exclamation-sign rediconcolor icon-2x\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[INFO\] (.+)<\/p>/)     { "<div class=\"alert-box\"><i class=\"fa fa-exclamation-triangle\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[SUCCESS\] (.+)<\/p>/)  { "<div class=\"alert-box success\"><i class=\"fa fa-thumbs-o-up\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[WARN\] (.+)<\/p>/)     { "<div class=\"alert-box alert\"><i class=\"fa fa-warning\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[NOTE\] (.+)<\/p>/)     { "<div class=\"alert-box secondary\"><i class=\"fa fa-info-circle\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[DOCS\] (.+)<\/p>/)     { "<div class=\"alert-box docs\"><i class=\"fa fa-book\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[CONCEPT\] (.+)<\/p>/)  { "<div class=\"alert-box concept\"><i class=\"fa fa-info-circle blueiconcolor\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[SIDEBAR\] (.+)<\/p>/)  { "<div class=\"alert-box sidebar\"><i class=\"fa fa-comment blueiconcolor fa-2x\"></i> #{$1}</div>" }
+    content.gsub!(/<p>\[COMMENT\] (.+)<\/p>/)  { "<div class=\"alert-box comment\"><i class=\"fa fa-exclamation-triangle rediconcolor fa-2x\"></i> #{$1}</div>" }
 
   end
 
@@ -68,7 +68,7 @@ module ZurbFoundation
   end
 
   def videos
-    content.gsub!(/\[VIDEO ([\w\d\:\/\.]+)\]/) { "<a href=\"#{$1}\" target=\"_TOP\"><i class=\"icon-youtube-sign\"></i></a>" }
+    content.gsub!(/\[VIDEO ([\w\d\:\/\.]+)\]/) { "<a href=\"#{$1}\" target=\"_TOP\"><i class=\"fa fa-youtube\"></i></a>" }
   end
 
   def render(string)
