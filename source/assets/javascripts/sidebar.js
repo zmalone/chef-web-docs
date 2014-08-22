1e7f33373034a8c0fd90157cfdbeedb2be9c305e
Andrew Tat <tat@andrewtat.com>
2014-08-22 14:29:13 -0700

    Fix initial positioning of the sidebar

--- source/assets/javascripts/sidebar.js
+++ source/assets/javascripts/sidebar.js
@@ -6,15 +6,15 @@ $('.accordion').on('click', '.title a', function (event) {
 (function() {
   var sidebar = $('#sidebar');
   var headerHeight = $('#header').height();
-  var initialSidebarOffset = sidebar.offset().top;
+  var initialContentOffset = $('section.row').offset().top;
 
   // Makes the sidebar slide along with the content while the window scrolls.
   $(window).scroll(function() {
     var top = $(window).scrollTop();
     if(top > headerHeight) {
-      sidebar.css("top", initialSidebarOffset - headerHeight);
+      sidebar.css("top", initialContentOffset - headerHeight);
     } else {
-      sidebar.css("top", -1 * top + initialSidebarOffset);
+      sidebar.css("top", -1 * top + initialContentOffset);
     }
   });
 }());
