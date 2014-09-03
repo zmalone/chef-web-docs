447e097d883fec28f44406b4edb866cb24317300
tpetchel <tpetchel@gmail.com>
2014-09-02 23:36:00 -0700

    Fix terminal corners and Metro IE
    1. On Windows, remove rounded corners from terminal windows.
    2. Add Win32 style explicitly so that it works on desktop and Metro IE.
    
    Tested on Mac (Safari, Chrome, Firefox), Windows (IE, MoBro, Chrome, Firefox), and Ubuntu (Firefox)

--- source/assets/javascripts/platform-window.js
+++ source/assets/javascripts/platform-window.js
@@ -1,1 +1,4 @@
-$('.window').addClass(navigator.platform);
+// Override with the Win32 styles if we're running on Windows
+if (navigator.appVersion.indexOf("Win") !== -1) {
+    $('.window').addClass("Win32");
+}
