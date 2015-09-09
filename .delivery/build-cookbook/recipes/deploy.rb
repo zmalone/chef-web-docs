9d62fb6d79059b88345aada80aed2a2da3c33d9e
Christopher Webber <cwebberops@gmail.com>
2015-09-09 06:53:50 -0700

    Add all the redirects!

--- .delivery/build-cookbook/recipes/deploy.rb
+++ .delivery/build-cookbook/recipes/deploy.rb
@@ -38,20 +38,20 @@ execute "upload the site" do
   cwd File.join(node['delivery_builder']['repo'], 'build')
 end
 
-#include_recipe 'build-cookbook::_install_dependencies'
+include_recipe 'build-cookbook::_install_dependencies'
 
-#execute 'create redirects' do
-#  command "bundle exec rake build_redirects"
-#  cwd node['delivery_builder']['repo']
-#  user node['delivery_builder']['build_user']
-#  environment(
-#    'PATH' => '/opt/chef/embedded/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games',
-#    'S3_BUCKET' => bucket_name,
-#    'AWS_ACCESS_KEY_ID' => aws_creds['access_key_id'],
-#    'AWS_SECRET_ACCESS_KEY' => aws_creds['secret_access_key']
-#  )
-#  sensitive true
-#end
+execute 'create redirects' do
+  command "bundle exec rake build_redirects"
+  cwd node['delivery_builder']['repo']
+  user node['delivery_builder']['build_user']
+  environment(
+    'PATH' => '/opt/chef/embedded/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games',
+    'S3_BUCKET' => bucket_name,
+    'AWS_ACCESS_KEY_ID' => aws_creds['access_key_id'],
+    'AWS_SECRET_ACCESS_KEY' => aws_creds['secret_access_key']
+  )
+  sensitive true
+end
 
 fastly_service = fastly_service fqdn do
   action :purge_all
