e66f3e605a421f690677a51c4ed9a917c374d035
Joshua Padgett <jpadgett@chef.io>
2015-12-03 12:10:29 -0700

    Fix route53 config
    old_learn_fqdn was using the wrong domain.
    Refactor to use constants and helper methods

--- .delivery/build-cookbook/recipes/_dns.rb
+++ .delivery/build-cookbook/recipes/_dns.rb
@@ -1,24 +1,13 @@
 load_delivery_chef_config
 aws_creds = encrypted_data_bag_item_for_environment('cia-creds','chef-secure')
 
-site_name = 'learn'
-domain_name = 'chef.io'
-
-if node['delivery']['change']['stage'] == 'delivered'
-  bucket_name = node['delivery']['change']['project'].gsub(/_/, '-')
-  fqdn = "#{site_name}.#{domain_name}"
-else
-  bucket_name = "#{node['delivery']['change']['project'].gsub(/_/, '-')}-#{node['delivery']['change']['stage']}"
-  fqdn = "#{site_name}-#{node['delivery']['change']['stage']}.#{domain_name}"
-end
-
 route53_record fqdn do
   name "#{fqdn}."
   value 'g.global-ssl.fastly.net'
   aws_access_key_id aws_creds['access_key_id']
   aws_secret_access_key aws_creds['secret_access_key']
   type 'CNAME'
-  zone_id aws_creds['route53'][domain_name]
+  zone_id aws_creds['route53'][DOMAIN_NAME]
   sensitive true
 end
 
@@ -28,7 +17,7 @@ route53_record old_learn_fqdn do
   aws_access_key_id aws_creds['access_key_id']
   aws_secret_access_key aws_creds['secret_access_key']
   type 'CNAME'
-  zone_id aws_creds['route53'][domain_name]
+  zone_id aws_creds['route53'][OLD_DOMAIN_NAME]
   sensitive true
 end
 
