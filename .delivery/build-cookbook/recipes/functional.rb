b8af0626222d48ec97433863103dbd04e4dcdcb6
Joshua Padgett <jpadgett@chef.io>
2016-05-19 14:24:14 -0700

    Build Cookbook Cleanup
    CIA-649 - load_delivery_chef_config instead of client_mode
    CIA-663 - Remove Slack notifications
    CIA-699 - Remove github recipes

--- .delivery/build-cookbook/recipes/functional.rb
+++ .delivery/build-cookbook/recipes/functional.rb
@@ -1,16 +1,6 @@
-
-include_recipe 'build-cookbook::_handler'
 include_recipe 'chef-sugar::default'
 
-Chef_Delivery::ClientHelper.enter_client_mode_as_delivery
-
-slack_creds = encrypted_data_bag_item_for_environment('cia-creds','slack')
-
-if ['union', 'rehearsal', 'delivered'].include?(node['delivery']['change']['stage'])
-  slack_channels = slack_creds['channels'].push('#operations').push('#learnchef')
-else
-  slack_channels = slack_creds['channels'].push('#learnchef')
-end
+load_delivery_chef_config
 
 site_name = 'learn'
 domain_name = 'chef.io'
@@ -27,24 +17,4 @@ execute 'run linkchecker' do
   command "linkchecker -t 6 --recursion-level 3 --no-warnings --ignore-url 'package.url' --ignore-url 'glyphicons' https://#{fqdn}/"
   cwd node['delivery_builder']['repo']
   user node['delivery_builder']['build_user']
-end
-
-case node['delivery']['change']['stage']
-when 'acceptance'
-  chef_slack_notify 'Notify Slack' do
-    channels slack_channels
-    webhook_url slack_creds['webhook_url']
-    username slack_creds['username']
-    message "*[#{node['delivery']['change']['project']}] (#{node['delivery']['change']['stage']}:#{node['delivery']['change']['phase']})* <a href=\"https://#{fqdn}\">https://#{fqdn}</a> is now ready for delivery! Please visit <a href=\"#{change_url}\">Deliver it!</a>"
-    sensitive true
-  end
-
-when 'delivered'
-  chef_slack_notify 'Notify Slack' do
-    channels slack_channels
-    webhook_url slack_creds['webhook_url']
-    username slack_creds['username']
-    message "*[#{node['delivery']['change']['project']}] (#{node['delivery']['change']['stage']}:#{node['delivery']['change']['phase']})* <a href=\"https://#{fqdn}\">https://#{fqdn}</a> is now Delivered!"
-    sensitive true
-  end
 end
