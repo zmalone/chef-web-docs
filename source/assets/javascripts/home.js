1e4871fcd46a8fc3eb539286e0d799977163998a
Maggie Walker <magwalk@gmail.com>
2016-06-10 11:15:25 -0700

    Add Community Section to homepage

--- source/assets/javascripts/home.js
+++ source/assets/javascripts/home.js
@@ -2,29 +2,31 @@ $(document).ready(function() {
   var $titles = $(".learn-section-title a");
   var $description = $(".lg-description");
 
-  $titles.hover( 
+  $titles.hover(
     function() {
-      $titles.find(".learn-section-icon").removeClass("fade-in"); 
+      $titles.find(".learn-section-icon").removeClass("fade-in");
 
       if ($(window).width() > 800) {
         var $this = $(this);
 
         if ( $(this).parents(".tutorials-title").length)  {
-          $description.removeClass("skills-active docs-active training-active")
+          $description.removeClass("skills-active docs-active training-active community-active")
             .addClass("tutorials-active");
 
         } else if ( $(this).parents(".skills-title").length) {
-          $description.removeClass("tutorials-active docs-active training-active")
+          $description.removeClass("tutorials-active docs-active training-active community-active")
             .addClass("skills-active");
 
         } else if ( $(this).parents(".docs-title").length) {
-          $description.removeClass("skills-active tutorials-active training-active")
+          $description.removeClass("skills-active tutorials-active training-active community-active")
             .addClass("docs-active");
 
         } else if ( $(this).parents(".training-title").length) {
-          $description.removeClass("skills-active docs-active tutorials-active")
+          $description.removeClass("skills-active docs-active tutorials-active community-active")
             .addClass("training-active");
-
+        } else if ( $(this).parents(".community-title").length) {
+          $description.removeClass("skills-active docs-active tutorials-active training-active")
+            .addClass("community-active");
         }
       }
   }, function() {
