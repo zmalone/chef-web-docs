cf6d553e5f8677102e3c03ca06ac9ac0000dfb9d
tpetchel <tpetchel@gmail.com>
2016-08-12 16:12:04 -0400

    BYO system, tutorial 1, RHEL

--- lib/helpers/snippet_helpers.rb
+++ lib/helpers/snippet_helpers.rb
@@ -4,7 +4,7 @@ module SnippetHelpers
     concat(File.read(Pathname.new(path).cleanpath))
   end
 
-  def command_snippet(page: nil, path:, workstation: nil, features: [:stdin, :stdout])
+  def command_snippet(page: nil, path:, workstation: nil, replace_prompt: nil, features: [:stdin, :stdout])
     workstation ||= page.data.snippet_workstation if page
     if page
       path = File.join(page.data.snippet_path, path + "-#{workstation}")
@@ -14,8 +14,9 @@ module SnippetHelpers
     render_snippet(path) do |_metadata|
       [*features].map do |feature|
         content = IO.read(File.join('snippets', path, feature.to_s))
-        if feature == :stdin && page && page.data.replace_prompt
-          content.sub!(/^.+\$/, page.data.replace_prompt)
+        if feature == :stdin && (replace_prompt || (page && page.data.replace_prompt))
+          replace_prompt ||= current_page.data.replace_prompt
+          content.sub!(/^.+\$/, replace_prompt)
         end
         content
       end.join
