6ed98ed2c12d950cc3fb7ccb18df0038fda19e9a
tpetchel <tpetchel@gmail.com>
2016-10-07 07:57:11 -0400

    Seed with snippets

--- snippets/manage-a-node/rhel/aws-automate/set-up-your-chef-server/generate-knife-config/knife-rb/knife.rb
+++ snippets/manage-a-node/rhel/aws-automate/set-up-your-chef-server/generate-knife-config/knife-rb/knife.rb
@@ -3,5 +3,5 @@ log_level                 :info
 log_location              STDOUT
 node_name                 "delivery"
 client_key                "#{current_dir}/delivery.pem"
-chef_server_url           "https://ec2-54-82-164-21.compute-1.amazonaws.com/organizations/4thcoffee"
+chef_server_url           "https://ec2-52-87-190-191.compute-1.amazonaws.com/organizations/4thcoffee"
 cookbook_path             ["#{current_dir}/../cookbooks"]
