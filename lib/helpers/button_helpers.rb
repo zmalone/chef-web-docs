b1f513c9e9dc68d03a9756f8f384d5208ed67f46
tpetchel <tpetchel@gmail.com>
2016-12-07 08:45:38 -0500

    Make logos larger

--- lib/helpers/button_helpers.rb
+++ lib/helpers/button_helpers.rb
@@ -1,9 +1,10 @@
 module ButtonHelpers
-  def platform_button(href, logo, text)
+  def platform_button(href, logo, text, logo_width)
+    logo_width = 25 if logo_width == -1
     concat(
       content_tag(:a, href: href) do
         content_tag(:span, class: 'button radius cta', style:"width: 300px;") do
-          image_tag(logo, style:"display:inline; margin-top: 0px; margin-bottom: 0px; margin-right: 7px; box-shadow:none;", width: 20) +
+          image_tag(logo, style:"display:inline; margin-top: 0px; margin-bottom: 0px; margin-right: 7px; box-shadow:none;", width: logo_width) +
           content_tag(:span, style: 'vertical-align:middle;') do
             text + " <i class='fa fa-angle-double-right'></i>"
           end
