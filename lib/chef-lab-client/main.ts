2818b066af149df0f40eaa4ce08caaf296d0c920
Benjamin Shell <benjaminshell@gmail.com>
2017-02-28 15:49:51 -0800

    Refactor of existing manage-a-node content to new layout

--- lib/chef-lab-client/main.ts
+++ lib/chef-lab-client/main.ts
@@ -11,6 +11,9 @@ try {
     console.error(e);
   });
 } catch (e) {
+  // TODO: Log these (and other) errors somewhere.
+  // With progressive enhancement of an HTML site, if Angular were to quietly fail it would still
+  // allow a user to go through the content, but it would not track the user's progress.
   console.log('Bootstrap error', e);
   // Fallback to static HTML page without Angular enhancements
   const appRoot = window.document.getElementsByTagName('app-root');
