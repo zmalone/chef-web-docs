aaea569a58cdb22769b63f28dd30ccffe60b73dd
tpetchel <tpetchel@gmail.com>
2017-03-01 22:11:52 -0500

    Update Chef Automate on-prem snippets
    + Chef server 12.13
    + Remove 4thcoffee org & user

--- snippets/manage-a-node/rhel/aws-automate/set-up-your-chef-server/generate-knife-config/knife-rb/knife.rb
+++ snippets/manage-a-node/rhel/aws-automate/set-up-your-chef-server/generate-knife-config/knife-rb/knife.rb
@@ -3,5 +3,5 @@ log_level                 :info
 log_location              STDOUT
 node_name                 "delivery"
 client_key                "#{current_dir}/delivery.pem"
-chef_server_url           "https://ec2-54-90-95-197.compute-1.amazonaws.com/organizations/automate"
+chef_server_url           "https://ec2-174-129-86-215.compute-1.amazonaws.com/organizations/automate"
 cookbook_path             ["#{current_dir}/../cookbooks"]
