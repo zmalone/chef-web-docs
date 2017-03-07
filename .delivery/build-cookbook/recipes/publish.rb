3f7e8771a24bd87ccec0f3a3698fe7f3559ccde5
Christopher Webber <cwebber@chef.io>
2017-03-07 10:10:59 -0800

    Deploy changes and add npm
    This is the merge that has all of the new content and added angular
    bits.
    
    Signed-off-by: Christopher Webber <cwebber@chef.io>

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -7,7 +7,9 @@ execute 'build the site' do
   environment(
     'PATH' => '/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games',
     'HOME' => node['delivery']['workspace']['cache'],
-    'CHEF_LAB_URL' => 'https://lab.chef.io'
+    'CHEF_LAB_URL' => 'https://lab.chef.io',
+    'NODE_ENV' => 'production',
+    'API_ENDPOINT' => 'http://learn-chef-api-dev.wirestone.net'
   )
   cwd node['delivery']['workspace']['repo']
 end
