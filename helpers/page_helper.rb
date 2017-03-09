20e4d0339b3d3f5de4d0b0566b9578f695267749
Benjamin Shell <benjaminshell@gmail.com>
2017-03-09 14:27:09 -0800

    Fix page helper issues with missing pages and complex nav trees

--- helpers/page_helper.rb
+++ helpers/page_helper.rb
@@ -2,29 +2,45 @@ module PageHelper
   # Remove additional extensions from the filename before calling
   # E.g. index.html -> index.html, index.html.md -> index.html, index.html.md.erb -> index.html
   def sanitize_path(path)
-    path.sub(/(\.[\w]+)[\.\w]*$/, '\\1')
+    path.sub(/(\.[\w]+)[\.\w]*$/, '\\1') if path
   end
 
   # Call sitemap.find_resource_by_path after sanitizing path
   def find_page_by_path(filepath)
     path = sanitize_path(filepath)
-    page = sitemap.find_resource_by_path(sanitize_path(path))
-    logger.warn "WARN: Unable to find page #{path}" if page.blank?
+    page = sitemap.find_resource_by_path(path)
+    logger.warn "WARN: Unable to find page #{filepath}" if page.blank?
     page
   end
 
   def find_pages_by_folder(folder)
     # logger.info "Find page in folder '#{folder}'"
+    pages = []
     data.tree[folder].map do |item|
-      item.last.values.first
+      extract_filepaths_from_tree(item.last).each do |filepath|
+        pages << find_page_by_path(filepath)
+      end
     end
+    pages.compact
+  end
+
+  def extract_filepaths_from_tree(hash)
+    filepaths = []
+    values = hash.to_hash.values
+    values.each do |val|
+      if val.respond_to? :to_hash
+        filepaths.concat(extract_filepaths_from_tree(val))
+      else
+        filepaths << val
+      end
+    end
+    filepaths
   end
 
   # Find a page by the ID attribute in the frontmatter data
   # TODO: Do some profiling on this, and consider adding caching if there's a performance problem
   def find_page_by_id(folder, id)
-    page = find_pages_by_folder(folder).map { |item|
-      page = find_page_by_path(item)
+    page = find_pages_by_folder(folder).map { |page|
       (!page.blank? && page.data.id == id) ? page : nil
     }.compact.first
     logger.warn "WARN: Unable to find page '#{id}' in '#{folder}'" if page.blank?
