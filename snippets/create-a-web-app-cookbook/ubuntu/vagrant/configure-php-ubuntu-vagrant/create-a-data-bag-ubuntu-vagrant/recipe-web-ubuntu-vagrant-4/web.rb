25145fc05b76537acd6b29ee5a7f2ae207b3fa15
tpetchel <tpetchel@gmail.com>
2017-03-30 13:44:52 -0400

    Add snippets

---
+++ snippets/create-a-web-app-cookbook/ubuntu/vagrant/configure-php-ubuntu-vagrant/create-a-data-bag-ubuntu-vagrant/recipe-web-ubuntu-vagrant-4/web.rb
@@ -0,0 +1,4 @@
+# Install the mod_php5 Apache module.
+httpd_module 'php5' do
+  instance 'default'
+end
---
+++ snippets/create-a-web-app-cookbook/ubuntu/vagrant/create-the-cookbook-ubuntu-vagrant/create-lamp_customers-cookbook-ubuntu-vagrant/metadata-ubuntu-vagrant-4/metadata.rb
@@ -0,0 +1,11 @@
+name 'lamp_customers'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures lamp_customers'
+long_description 'Installs/Configures lamp_customers'
+version '0.1.0'
+issues_url 'https://github.com/learn-chef/lamp_customers/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/lamp_customers' if respond_to?(:source_url)
+
+depends 'lamp'
