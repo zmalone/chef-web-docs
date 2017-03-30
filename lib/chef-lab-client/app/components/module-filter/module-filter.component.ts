d510e54506ac15e21f065ad619b5a58191e2ef0b
Benjamin Shell <benjaminshell@gmail.com>
2017-03-30 08:29:45 -0700

    Refactor getTagList method using map/filter/reduce methods

--- lib/chef-lab-client/app/components/module-filter/module-filter.component.ts
+++ lib/chef-lab-client/app/components/module-filter/module-filter.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core'
 import {IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts} from 'angular-2-dropdown-multiselect'
 import {ModuleFilterService} from '../../services/module-filter.service'
+interface TagObject { id: string, name: string }
 
 @Component({
   selector: 'module-filter',
@@ -42,24 +43,17 @@ export class ModuleFilterComponent implements OnInit {
     this.ModuleFilter.setTagInfo(this.optionsModel)
   }
 
-  public getTagList() {
-    const nodes = document.querySelectorAll('div.module-tile')
-    const tagList = []
-    const tagOption = []
-    if (nodes != null) {
-      const nodeList = Array.prototype.slice.call(nodes, 0)
-      nodeList.map(function (node) {
-        const tags = JSON.parse(node.attributes.getNamedItem('data-tags').nodeValue.replace(/'/g, '"'))
-        if (tags !== null) {
-          tags.map(function (tag) {
-            if (tagList.indexOf(tag) <= -1) {
-              tagList.push(tag)
-              tagOption.push({name: tag, id: tag})
-            }
-          })
-        }
-      })
-    }
-    return tagOption
+  public getTagList(): Array<TagObject> {
+    const nodes = document.querySelectorAll('div.module-tile') || []
+    const nodeList = Array.prototype.slice.call(nodes, 0) || []
+    return nodeList.map(node => {
+      return JSON.parse(node.attributes.getNamedItem('data-tags').nodeValue.replace(/'/g, '"')) || []
+    }).reduce((allTags, nodeTags) => {
+      return allTags.concat(nodeTags)
+    }).filter((tag, index, tags) => {
+      return tags.indexOf(tag) === index
+    }).sort().map(tag => {
+      return { id: tag, name: tag }
+    })
   }
 }
