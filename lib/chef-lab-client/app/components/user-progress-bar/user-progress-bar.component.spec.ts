79c05c4aacf345f7c5d522236a735c276afa0a38
Benjamin Shell <benjaminshell@gmail.com>
2017-03-31 23:06:40 -0700

    Get one test passing

--- lib/chef-lab-client/app/components/user-progress-bar/user-progress-bar.component.spec.ts
+++ lib/chef-lab-client/app/components/user-progress-bar/user-progress-bar.component.spec.ts
@@ -3,15 +3,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { By } from '@angular/platform-browser'
 import { DebugElement } from '@angular/core'
 
+import { SiteDataService } from '../../services/site-data.service'
+import { ProgressService } from '../../services/progress.service'
+import { UserProfileService } from '../../services/user-profile.service'
 import { UserProgressBarComponent } from './user-progress-bar.component'
+import { Angular2TokenService } from 'angular2-token'
+import MockAngular2TokenService from '../../../mock/MockAngular2TokenService'
+import MockSiteDataService from '../../../mock/MockSiteDataService'
 
 describe('UserProgressBarComponent', () => {
+
   let component: UserProgressBarComponent
   let fixture: ComponentFixture<UserProgressBarComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UserProgressBarComponent ],
+      providers: [
+        ProgressService,
+        UserProfileService,
+        { provide: Angular2TokenService, useClass: MockAngular2TokenService },
+        { provide: SiteDataService, useClass: MockSiteDataService },
+      ],
     })
       .compileComponents()
   }))
---
+++ lib/chef-lab-client/mock/MockAngular2TokenService.ts
@@ -0,0 +1,2 @@
+export default class MockAngular2TokenService {
+}
