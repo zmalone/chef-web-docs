5b0105ad2c6919824d45b71d65803f9861738289
Manish Tripathi <manish.tripathi@intimetec.com>
2017-04-05 20:01:12 +0530

    worked on progress nav link

--- source/assets/javascripts/home.js
+++ source/assets/javascripts/home.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   var $titles = $(".home--section--title");
   var $description = $(".home--descriptions");
+  if (window.location.href.match('progress')) {
+    $('#progressTab').trigger('click')
+  }
   $titles.hover(
     function() {
       $titles.find(".home--section--icon").removeClass("fade-in");
