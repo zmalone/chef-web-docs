4ac19dc9aca584b493aba80979eac894fac69bc8
Benjamin Shell <benjaminshell@gmail.com>
2017-04-10 15:02:21 -0700

    Restore Ruby script line numbers
    Line numbers were missing because this is considered a legacy feature of the Rouge gem, which can fortunately be easily re-enabled. There was also an issue where <pre> tags were being confused with paragraph tags in the Angularize helpers, which was breaking the formatting of line numbers once they were re-enabled.

--- lib/markdown.rb
+++ lib/markdown.rb
@@ -163,7 +163,7 @@ module ZurbFoundation
     content.gsub!('<p><h', '<h') # A paragraph shouldn't be opened right before a heading tag
     content.gsub!('</div></p>', '</p></div>') # The page_nav_helpers.rb next_page helper results in invalid closing tag order
     in_paragraph = false
-    content.gsub!(/<(\/)?(p|P)[^>]*>/) { |match|
+    content.gsub!(/<(\/)?(p|P)([\s]+[^>]*)*>/) { |match|
       # Remove closing </p> tags if we're NOT in a paragraph, and opening <p> tags if we ARE in a paragraph
       next '' if in_paragraph ^ $1
       in_paragraph = !$1
