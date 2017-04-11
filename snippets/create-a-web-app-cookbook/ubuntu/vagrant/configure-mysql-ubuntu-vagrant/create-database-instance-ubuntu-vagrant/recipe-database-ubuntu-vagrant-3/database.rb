ef976152d478597f44bc430fcf4abe0e692a5813
tpetchel <tpetchel@gmail.com>
2017-04-11 12:42:18 -0400

    Integrate changes for Chef 13

--- snippets/create-a-web-app-cookbook/ubuntu/vagrant/configure-mysql-ubuntu-vagrant/create-database-instance-ubuntu-vagrant/recipe-database-ubuntu-vagrant-3/database.rb
+++ snippets/create-a-web-app-cookbook/ubuntu/vagrant/configure-mysql-ubuntu-vagrant/create-database-instance-ubuntu-vagrant/recipe-database-ubuntu-vagrant-3/database.rb
@@ -1,7 +1,7 @@
 mysql_connection_info = {
-  :host     => '127.0.0.1',
-  :username => 'root',
-  :password => passwords['root_password']
+  host: '127.0.0.1',
+  username: 'root',
+  password: passwords['root_password']
 }
 
 # Create the database instance.
